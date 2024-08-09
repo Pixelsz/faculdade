543d9e5b8316f42229063a7152fdbacfe64a459f
Pixelsz <pinkdarin@gmail.com>
2024-08-09 12:31:23 -0300

    fix: now shows the proper values

--- exemploreal.c
+++ exemploreal.c
@@ -3,11 +3,11 @@
 int main()
 {
     int qtd = 25;
-    int preco_unidade = 10;
+    float preco_unidade = 10.99;
     int preco_total = qtd * preco_unidade;
     char moeda[] = "R$";
 
     printf("Número de itens: %d\n", qtd);
-    printf("Preço de cada unidade: %d\n", preco_unidade);
-    printf("Valor total:  %d %s\n", preco_total, moeda);
+    printf("Preço de cada unidade: %.2f %s\n", preco_unidade, moeda);
+    printf("Valor total:  %.2f %s\n", preco_total, moeda);
 }
